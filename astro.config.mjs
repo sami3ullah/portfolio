fc51c1fa6210e0a9ce2e8e68507bad05c5661d39
Muhammad Samiullah <msami398@gmail.com>
2023-09-05 13:03:55 +0500

    basic connection with storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
 
 // https://astro.build/config
-export default defineConfig({});
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: 'storyblok/BlogPost',
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: 'eu', // optional,  or 'eu' (default)
+      },
+    })
+  ],
+});
