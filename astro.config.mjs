770d50418e859a6b0b3d59896b0234370f16dece
Muhammad Samiullah <msami398@gmail.com>
2023-11-19 03:09:16 +0500

    feature: Added google analytics + og description, title and image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import { loadEnv } from "vite";
+import partytown from '@astrojs/partytown'
 import sanity from "@sanity/astro";
 // import react from "@astrojs/react";
 const {
@@ -11,7 +12,13 @@ const {
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sanity({
+  integrations: [tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      }
+    }), 
+    sanity({
     projectId: PUBLIC_SANITY_PROJECT_ID,
     dataset: PUBLIC_SANITY_DATASET,
     useCdn: true,
